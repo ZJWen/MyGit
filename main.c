6ed879a8f8fe221e2bf4a59685e94ac8c209f730
zjw <1301447557@qq.com>
2017-11-15 18:18:14 +0800

    首次提交

--- main.c
+++ main.c
@@ -1,6 +1,14 @@
+#include<math.h>
 #include<stdio.h>
 int main(int argc,char const *argv[] )
 {
-	printf("hello\n");
+	//printf("%d",pow(2,3));
+	int arr[]={1,2,3,4,5,6,7,8};
+	int arrsize=sizeof arr;
+	int length =arrsize/4;
+	int i;
+
+
+	printf("%d",arrsize);
 	return 0;
 }
